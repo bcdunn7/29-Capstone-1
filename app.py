ac2b86fb286f7192310cc999f0782d888ca0829a
Bryce <bryce.cc.dunn@gmail.com>
2021-07-08 15:30:49 -0500

    Base and user auth routing started. Initial templates created.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///influenceF1'
@@ -15,3 +15,55 @@ app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
+CURR_USER_KEY = "current_user"
+
+
+# ***********************************************
+# User signup/login/logout
+
+@app.before_request
+def add_user_to_g():
+    """If logged in, add user to Flask global obj."""
+
+    if CURR_USER_KEY in session: 
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def session_login(user):
+    """'Log in' user: store user id in session."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def session_logout():
+    """'Log out' user: delete user id from session."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+# ***********************************************
+# Base ROUTING
+
+@app.route('/')
+def homepage():
+    """Homepage with info and demo of app."""
+
+    return render_template('home.html')
+
+
+@app.route('/how-it-works')
+def tutorial():
+    """Tutorial page explaining the app and how the simulator works."""
+
+    return render_template('tutorial.html')
+
+
+@app.route('/simulator')
+def simulator():
+    """Main functionality page of application: show simulator."""
+
+    return render_template('simulator.html')
