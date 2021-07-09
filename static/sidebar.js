576b706518c52f4163c933ec3505e112eeb99cb6
Bryce <bryce.cc.dunn@gmail.com>
2021-07-09 13:57:54 -0500

    added basic sidebar for navigation

---
+++ static/sidebar.js
@@ -0,0 +1,8 @@
+// toggle sidebar 
+$('#sidebar-toggle-open').on('click', function() {
+    $('#sidebar').addClass('active') 
+})
+
+$('#sidebar-toggle-close').on('click', function() {
+    $('#sidebar').removeClass('active') 
+})
