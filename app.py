f40b604b65250a702d72412e11899720895b2e1e
Bryce <bryce.cc.dunn@gmail.com>
2021-08-01 16:32:23 -0500

    add hreoku postgres URL

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 db.create_all()
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'b/xc3/x8a/xdc/x84/x98/x05/xfc/xaa/xb5/xde/xa9')
 
 CURR_USER_KEY = "current_user"
 
